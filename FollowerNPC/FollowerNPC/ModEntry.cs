ac2d968e5e7aac743ba4857552f858dfffb4dfdc
EKomperud <ericmkomperud@gmail.com>
2018-10-25 16:56:21 -0600

    FollowerNPC started

---
+++ FollowerNPC/FollowerNPC/ModEntry.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using Harmony;
+using StardewModdingAPI;
+using StardewValley;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI.Events;
+
+namespace FollowerNPC
+{
+    public class ModEntry : Mod
+    {
+        public static ModConfig config;
+        public static IMonitor monitor;
+        public bool spawned;
+        public NPC whiteBox;
+        public float whiteBoxSpeed;
+        public float whiteBoxAnimationSpeed;
+        public float whiteBoxFollowThreshold;
+        public Vector2 whiteBoxLastMovementDirection;
+        public Vector2 whiteBoxLastPosition;
+        public bool whiteBoxMovedLastFrame;
+        public bool whiteBoxNeedsWarp;
+        public int whiteBoxWarpTimer;
+        public aStar whiteBoxAStar;
+        public Queue<Vector2> whiteBoxPath;
+        public Vector2 whiteBoxPathNode;
+        public float whiteBoxPathfindNodeGoalTolerance;
+        public bool whiteBoxFollow;
+
+        public Farmer farmer;
+        public Vector2 farmerLastTile;
+
+        public override void Entry(IModHelper helper)
+        {
+            config = Helper.ReadConfig<ModConfig>();
+            monitor = Monitor;
+            whiteBoxFollowThreshold = 3;
+            whiteBoxPathfindNodeGoalTolerance = 0.1f;
+
+            HarmonyInstance harmony = HarmonyInstance.Create("Redwood.FollowerNPC");
+
+            ControlEvents.KeyReleased += ControlEvents_KeyReleased;
+            //GameEvents.UpdateTick += GameEvents_UpdateTick;
+            GameEvents.FourthUpdateTick += GameEvents_FourthUpdateTick;
+            SaveEvents.AfterLoad += SaveEvents_AfterLoad;
+            PlayerEvents.Warped += PlayerEvents_Warped;
+            MineEvents.MineLevelChanged += MineEvents_MineLevelChanged;
+        }
+
+        private void ControlEvents_KeyReleased(object sender, EventArgsKeyPressed e)
+        {
+            //TO-DO: Utilize Path.Combine
+            if (!Context.IsWorldReady)
+                return;
+
+            if (e.KeyPressed == Keys.O && !spawned)
+            {
+                spawned = true;
+                AnimatedSprite sprite = new AnimatedSprite("Characters\\Abigail",0,Game1.tileSize / 4, (Game1.tileSize * 2)/4);
+                whiteBox = new NPC(sprite, Game1.player.Position, "SeedShop", 2, "Abigail", true, null, Game1.content.Load<Texture2D>("Portraits\\Abigail"));
+                //AnimatedSprite sprite = new AnimatedSprite("Characters\\Maru", 0, Game1.tileSize / 4, (Game1.tileSize * 2) / 4);
+                //whiteBox = new NPC(sprite, Game1.player.Position, "ScienceHouse", 2, "Maru", true, null, Game1.content.Load<Texture2D>("Portraits\\Maru"));
+                Game1.player.currentLocation.addCharacter(whiteBox);
+                //whiteBoxAStar = new aStar(farmer.currentLocation);
+                whiteBox.showTextAboveHead("Hey " + farmer.Name + "!", -1, 2, 3000, 0);
+                whiteBoxSpeed = 5f;
+                whiteBoxAnimationSpeed = 10f;
+                whiteBoxFollow = false;
+            }
+
+            else if (e.KeyPressed == Keys.P && spawned)
+            {
+                spawned = false;
+                Game1.removeCharacterFromItsLocation("Abigail");
+                //Game1.removeCharacterFromItsLocation("Maru");
+                whiteBox = null;
+            }
+
+            else if (e.KeyPressed == Keys.U && spawned)
+            {
+                monitor.Log(whiteBox?.currentLocation.Name + " : " + whiteBox?.getTileLocation());
+            }
+
+            else if (e.KeyPressed == Keys.I)
+            {
+                monitor.Log(farmer?.currentLocation.Name + " : " + farmer?.getTileLocation());
+            }
+
+            else if (e.KeyPressed == Keys.L && spawned)
+            {
+                Game1.warpCharacter(whiteBox, farmer.currentLocation, farmer.getTileLocation());
+            }
+
+            else if (e.KeyPressed == Keys.K && spawned)
+            {
+                whiteBoxFollow = !whiteBoxFollow;
+            }
+        }
+
+        //private void GameEvents_UpdateTick(object sender, EventArgs e)
+        //{
+        //    if (!Context.IsWorldReady || !(whiteBox != null) || !(farmer != null))
+        //        return;
+
+        //    FollowFarmer();
+        //}
+
+        private void GameEvents_FourthUpdateTick(object sender, EventArgs e)
+        {
+            if (!Context.IsWorldReady || !(whiteBox != null) || !(farmer != null))
+                return;
+
+            DelayedWarp();
+            if (whiteBoxFollow)
+                FollowFarmer();
+        }
+
+        private void SaveEvents_AfterLoad(object sender, EventArgs e)
+        {
+            farmer = Game1.player;
+            spawned = false;
+            whiteBox = null;
+        }
+
+        private void PlayerEvents_Warped(object sender, EventArgsPlayerWarped e)
+        {
+            if (!Context.IsWorldReady || !spawned || !(whiteBox != null) || !(farmer != null))
+                return;
+
+            whiteBoxAStar.gameLocation = farmer.currentLocation;
+            if (!farmer.isRidingHorse())
+                Game1.warpCharacter(whiteBox, farmer.currentLocation, farmer.getTileLocation());
+            else
+            {
+                whiteBoxNeedsWarp = true;
+                whiteBoxFollow = false;
+                whiteBoxWarpTimer = 4;
+            }
+        }
+
+        private void MineEvents_MineLevelChanged(object sender, EventArgsMineLevelChanged e)
+        {
+            if (!Context.IsWorldReady || !spawned || !(whiteBox != null) || !(farmer != null))
+                return;
+
+            Game1.warpCharacter(whiteBox, farmer.currentLocation, farmer.getTileLocation());
+        }
+
+        #region Helpers
+
+        private void FollowFarmer()
+        {
+            Point f = farmer.GetBoundingBox().Center;
+            Point w = whiteBox.GetBoundingBox().Center;
+            Vector2 diff = new Vector2(f.X, f.Y) - new Vector2(w.X, w.Y);
+            float diffLen = diff.Length();
+            if (diffLen > Game1.tileSize * whiteBoxFollowThreshold)
+            {
+                Vector2 farmerCurrentTile = farmer.getTileLocation();
+                if (farmerLastTile != farmerCurrentTile)
+                {
+                    whiteBoxPath = whiteBoxAStar.Pathfind(whiteBox.getTileLocation(), farmerCurrentTile);
+                    whiteBoxPathNode = whiteBoxPath.Dequeue();
+                }
+                if (whiteBoxPathNode != null)
+                {
+                    Point n = new Point((int)whiteBoxPathNode.X * Game1.tileSize, (int)whiteBoxPathNode.Y * Game1.tileSize);
+                    Vector2 nodeDiff = new Vector2(n.X, n.Y) - new Vector2(w.X, w.Y);
+                    float nodeDiffLen = nodeDiff.Length();
+                    while (nodeDiffLen <= whiteBoxPathfindNodeGoalTolerance)
+                    {
+                        if (whiteBoxPath.Count == 0)
+                            return;
+                        whiteBoxPathNode = whiteBoxPath.Dequeue();
+                        n = new Point((int)whiteBoxPathNode.X * Game1.tileSize, (int)whiteBoxPathNode.Y * Game1.tileSize);
+                        nodeDiff = new Vector2(n.X, n.Y) - new Vector2(w.X, w.Y);
+                        nodeDiffLen = nodeDiff.Length();
+                    }
+                    nodeDiff /= nodeDiffLen;
+                    whiteBox.xVelocity = nodeDiff.X * whiteBoxSpeed;
+                    whiteBox.yVelocity = -nodeDiff.Y * whiteBoxSpeed;
+                    whiteBox.faceDirection(GetFacingDirectionFromMovement(nodeDiff));
+                    SetMovementDirectionAnimation(whiteBox.FacingDirection);
+                    whiteBox.MovePosition(Game1.currentGameTime, Game1.viewport, whiteBox.currentLocation);
+                    whiteBoxLastMovementDirection = nodeDiff;
+
+                }
+                farmerLastTile = farmerCurrentTile;
+            }
+            else if (whiteBoxMovedLastFrame)
+            {
+                whiteBox.Sprite.faceDirectionStandard(GetFacingDirectionFromMovement(whiteBoxLastMovementDirection));
+                whiteBoxMovedLastFrame = false;
+            }
+        }
+
+        private void DelayedWarp()
+        {
+            if (whiteBoxNeedsWarp)
+                if (--whiteBoxWarpTimer <= 0)
+                {
+                    whiteBoxFollow = true;
+                    Game1.warpCharacter(whiteBox, farmer.currentLocation, farmer.getTileLocation());
+                    whiteBoxNeedsWarp = false;
+                }
+        }
+
+        private int GetFacingDirectionFromMovement(Vector2 movement)
+        {
+            int dir = 2;
+            if (Math.Abs(movement.X) > Math.Abs(movement.Y))
+                dir = movement.X > 0 ? 1 : 3;
+            else if (Math.Abs(movement.X) < Math.Abs(movement.Y))
+                dir = movement.Y > 0 ? 2 : 0;
+            return dir;
+        }
+
+        private void SetMovementDirectionAnimation(int dir)
+        {
+            switch (dir)
+            {
+                case 0:
+                    whiteBox.SetMovingOnlyUp();
+                    whiteBox.Sprite.AnimateUp(Game1.currentGameTime, (int)(whiteBoxSpeed * -whiteBoxAnimationSpeed), ""); break;
+                case 1:
+                    whiteBox.SetMovingOnlyRight();
+                    whiteBox.Sprite.AnimateRight(Game1.currentGameTime, (int)(whiteBoxSpeed * -whiteBoxAnimationSpeed), ""); break;
+                case 2:
+                    whiteBox.SetMovingOnlyDown();
+                    whiteBox.Sprite.AnimateDown(Game1.currentGameTime, (int)(whiteBoxSpeed * -whiteBoxAnimationSpeed), ""); break;
+                case 3:
+                    whiteBox.SetMovingOnlyLeft();
+                    whiteBox.Sprite.AnimateLeft(Game1.currentGameTime, (int)(whiteBoxSpeed * -whiteBoxAnimationSpeed), ""); break;
+
+            }
+        }
+
+        private bool FloatsApproximatelyEqual(float a, float b, float threshold)
+        {
+            return Math.Abs(a - b) < threshold;
+        }
+        #endregion
+    }
+}
+
---
+++ FollowerNPC/FollowerNPC/aStar.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using xTile.Dimensions;
+
+namespace FollowerNPC
+{
+    public class aStar
+    {
+        public aStar(GameLocation location)
+        {
+            gameLocation = location;
+        }
+
+        public GameLocation gameLocation
+        {
+            get { return gameLocation; }
+            set
+            {
+                gameLocation = value;
+                dimensions = new Vector2(gameLocation.map.Layers[0].LayerWidth, gameLocation.map.Layers[0].LayerHeight);
+            }
+        }
+        private Vector2 dimensions;
+        private Vector2 negativeOne = new Vector2(-1, -1);
+
+        public Queue<Vector2> Pathfind(Vector2 start, Vector2 goal)
+        {
+            PriorityQueue open = new PriorityQueue(new Node(null, start));
+            Dictionary<Vector2, float> closed = new Dictionary<Vector2, float>();
+            Vector2 mapDimensions = new Vector2(100, 100);
+            List<Node> path = new List<Node>();
+
+            while (open.Count != 0)
+            {
+                Node q = open.Dequeue();
+                Node[] successors = GetSuccessors(q);
+                foreach (Node successor in successors)
+                {
+                    if (!(successor != null))
+                        continue;
+
+                    if (successor.position == goal)
+                    {
+                        path = Reconstruct(successor);
+                        goto NodeConsolidation;
+                    }
+
+                    successor.g = q.g + EuclideanDistanceSquared(q.position, successor.position);
+                    bool locationOpen = gameLocation.isTileLocationOpen(new Location((int)successor.position.X, (int)successor.position.Y));
+                    //ModEntry.monitor.Log("null check & goal check & g set & locationOpen set");
+                    successor.h = EuclideanDistanceSquared(successor.position, goal) + (locationOpen ? 0f : mapDimensions.LengthSquared());
+                    successor.f = successor.g + successor.h;
+
+                    if (open.CheckValue(successor.position) < successor.f)
+                        break;
+
+                    if (closed.TryGetValue(successor.position, out float closedNodeCost) && closedNodeCost < successor.f)
+                        break;
+
+                    open.Enqueue(successor);
+                }
+            }
+            return null;
+            NodeConsolidation:
+            Queue<Vector2> consolidatedPath = new Queue<Vector2>();
+            foreach (Node node in path)
+            {
+                if (IsCorner(node.position))
+                    consolidatedPath.Enqueue(node.position);
+            }
+            return consolidatedPath;
+        }
+
+        private Vector2[] GetNeighbors(Vector2 tile)
+        {
+            float ppx = tile.X;
+            float ppy = tile.Y;
+            float mdx = dimensions.X;
+            float mdy = dimensions.Y;
+            Vector2[] successors = new Vector2[8];
+            successors[0] = ppy > 0 ? new Vector2(ppx, ppy - 1) : negativeOne;
+            successors[1] = ppy > 0 && ppx < mdx - 1 ? new Vector2(ppx + 1, ppy - 1) : negativeOne;
+            successors[2] = ppx < mdx - 1 ? new Vector2(ppx + 1, ppy) : negativeOne;
+            successors[3] = ppy < mdy - 1 && ppx < mdx - 1 ? new Vector2(ppx + 1, ppy + 1) : negativeOne;
+            successors[4] = ppy < mdy - 1 ? new Vector2(ppx, ppy + 1) : negativeOne;
+            successors[5] = ppy < mdy - 1 && ppx > 0 ? new Vector2(ppx - 1, ppy + 1) : negativeOne;
+            successors[6] = ppx > 0 ? new Vector2(ppx - 1, ppy) : negativeOne;
+            successors[7] = ppy > 0 && ppx > 0 ? new Vector2(ppx - 1, ppy - 1) : negativeOne;
+            return successors;
+        }
+
+        private Node[] GetSuccessors(Node parent)
+        {
+            float ppx = parent.position.X;
+            float ppy = parent.position.Y;
+            float mdx = dimensions.X;
+            float mdy = dimensions.Y;
+            Node[] successors = new Node[8];
+            successors[0] = ppy > 0 ? new Node(parent, new Vector2(ppx, ppy - 1)) : null;
+            successors[1] = ppy > 0 && ppx < mdx - 1 ? new Node(parent, new Vector2(ppx + 1, ppy - 1)) : null;
+            successors[2] = ppx < mdx - 1 ? new Node(parent, new Vector2(ppx + 1, ppy)) : null;
+            successors[3] = ppy < mdy - 1 && ppx < mdx - 1 ? new Node(parent, new Vector2(ppx + 1, ppy + 1)) : null;
+            successors[4] = ppy < mdy - 1 ? new Node(parent, new Vector2(ppx, ppy + 1)) : null;
+            successors[5] = ppy < mdy - 1 && ppx > 0 ? new Node(parent, new Vector2(ppx - 1, ppy + 1)) : null;
+            successors[6] = ppx > 0 ? new Node(parent, new Vector2(ppx - 1, ppy)) : null;
+            successors[7] = ppy > 0 && ppx > 0 ? new Node(parent, new Vector2(ppx - 1, ppy - 1)) : null;
+            return successors;
+        }
+
+        private List<Node> Reconstruct(Node goal)
+        {
+            Node iterator = goal;
+            List<Node> ret = new List<Node>();
+            ret.Add(goal);
+            while (iterator.parent != null)
+            {
+                ret.Add(iterator.parent);
+                iterator = iterator.parent;
+            }
+            return ret;
+        }
+
+        private bool IsCorner(Vector2 tile)
+        {
+            Vector2[] neighbors = GetNeighbors(tile);
+            bool[] cornersOccupied = new bool[4] {false, false, false, false};
+            for (int i = 1; i < 8; i+=2)
+            {
+                Location loc = new Location((int) (neighbors[i].X * Game1.tileSize), (int) (neighbors[i].Y * Game1.tileSize));
+                if (!gameLocation.isTileLocationOpen(loc) && neighbors[i] != negativeOne)
+                {
+                    Location n1 = new Location((int)(neighbors[i-1].X * Game1.tileSize), (int)(neighbors[i-1].Y * Game1.tileSize));
+                    Location n2 = new Location((int) (neighbors[i + 1 > 7 ? 0 : i + 1].X * Game1.tileSize),
+                        (int) (neighbors[i + 1 > 7 ? 0 : i + 1].Y * Game1.tileSize));
+                    if ((gameLocation.isTileLocationOpen(n1) && neighbors[i - 1] != negativeOne) &&
+                        (gameLocation.isTileLocationOpen(n2) && neighbors[i + 1 > 7 ? 0 : i + 1] != negativeOne))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private float ManhattanDistance(Vector2 a, Vector2 b)
+        {
+            return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+        }
+
+        private float EuclideanDistanceSquared(Vector2 a, Vector2 b)
+        {
+            return ((a.X - b.X) * (a.X - b.X)) + ((a.Y - b.Y) * (a.Y - b.Y));
+        }
+    }
+
+    class Node
+    {
+        public Node parent;
+        public Vector2 position;
+        public float f { get; set; }
+        public float g { get; set; }
+        public float h { get; set; }
+
+        public Node(Node parent, Vector2 position)
+        {
+            this.parent = parent;
+            this.position = position;
+        }
+    }
+
+    class PriorityQueue
+    {
+        Node[] items;
+        Dictionary<Vector2, int> positions;
+        public int Count { get; private set; }
+
+        public PriorityQueue(Node head)
+        {
+            items = new Node[100];
+            items[0] = null;
+            items[1] = head;
+            Count = 1;
+
+            positions = new Dictionary<Vector2, int>();
+            positions.Add(head.position, 1);
+        }
+
+        public void Enqueue(Node item)
+        {
+            int position;
+            if (!positions.TryGetValue(item.position, out position))
+            {
+                Count++;
+                if (Count >= items.Length / 2)
+                    ResizeItems();
+                items[Count] = item;
+                positions.Add(item.position, Count);
+                PercolateUp(Count);
+            }
+            else if (items[position].f > item.f)
+            {
+                items[position] = item;
+                PercolateUp(position);
+            }
+        }
+
+        public Node Dequeue()
+        {
+            Node ret = items[1];
+            items[1] = items[Count];
+            items[Count] = null;
+            positions.Remove(ret.position);
+            if (Count > 1)
+                positions[items[1].position] = 1;
+            PercolateDown();
+            Count--;
+            return ret;
+        }
+
+        public Node Peek()
+        {
+            return items[1];
+        }
+
+        public float CheckValue(Vector2 nodeValue)
+        {
+            if (positions.TryGetValue(nodeValue, out int itemPosition))
+                return items[itemPosition].f;
+            return float.PositiveInfinity;
+        }
+
+        private void PercolateUp(int index)
+        {
+            while (items[index / 2] != null)
+            {
+                Node child = items[index];
+                Node parent = items[index / 2];
+                if (child.f < parent.f)
+                {
+                    items[index] = parent;
+                    positions[parent.position] = index;
+                    items[index / 2] = child;
+                    positions[child.position] = index / 2;
+                    index = index / 2;
+                }
+                else
+                    return;
+            }
+        }
+
+        private void PercolateDown()
+        {
+            int index = 1;
+            int replaceIndex = 1;
+            while (index == replaceIndex)
+            {
+                Node child_L = items[(index * 2)];
+                Node child_R = items[(index * 2) + 1];
+                Node parent = items[index];
+                if (child_L != null && child_R != null)
+                    replaceIndex = child_L.f < child_R.f ? (index * 2) : (index * 2) + 1;
+                else if (child_L != null)
+                    replaceIndex = index * 2;
+                else
+                    return;
+                Node child = items[replaceIndex];
+                if (child.f < parent.f)
+                {
+                    items[replaceIndex] = parent;
+                    positions[parent.position] = replaceIndex;
+                    items[index] = child;
+                    positions[child.position] = index;
+                    index = replaceIndex;
+                }
+            }
+        }
+
+        private void ResizeItems()
+        {
+            Node[] newItems = new Node[items.Length * 2];
+            for (int i = 0; i < Count; i++)
+                newItems[i] = items[i];
+            items = newItems;
+        }
+    }
+}
